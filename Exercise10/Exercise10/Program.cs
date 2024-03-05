e5bbe6eaf2e5a0f4b9f57fce254a28c380d4f09b
adam ^.^ <145371242+deathgriips@users.noreply.github.com>
2024-03-05 13:14:43 -0500

    Started working on exercise 10

---
+++ Exercise10/Exercise10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Exercise10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*******************");
+            Console.WriteLine("Menu: ");
+            Console.WriteLine("1 - New Game");
+            Console.WriteLine("2 - Load Game");
+            Console.WriteLine("3 - Options");
+            Console.WriteLine("4 - Quit");
+            Console.WriteLine("*******************");
+
+            bool userInput = bool.Parse(Console.ReadLine());
+
+            // Declare while loop
+
+            while (userInput = 1 || 2 || 3)
+            {
+                Console.WriteLine("Loading...");
+            }
+        }
+    }
+}
